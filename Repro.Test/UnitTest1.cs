e75b02980e285990ff86c27b997f5cb08d288951
Andreas Gruenwald <andreas@grynwald.net>
2020-09-24 22:19:05 +0200

    Initial Commit

---
+++ Repro.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Repro.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var sut = new Class1();
+        }
+    }
+}
